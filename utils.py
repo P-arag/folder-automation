c18636ca7c72a1523de71272212be12a3cb1d978
parag <pal.parag257@gmail.com>
2021-02-28 14:00:38 +0530

    Improved and modular utility functions :triumph:

--- utils.py
+++ utils.py
@@ -1,41 +1,45 @@
 import os
 import shutil
+from os.path import *
 
 
 def move_file(src: str, where: str) -> None:
     print("Moving %s to %s" % (src, where))
-    print(os.listdir(os.path.expanduser(where)))
+    # print(os.listdir(os.path.expanduser(where)))
     if check_if_exists(src, where):
         print(f"Oops, {src} file already exists in {where}")
-        new_file_name = rename_if_present(src, where)
-        os.rename(src, new_file_name)
-        shutil.move(new_file_name, os.path.expanduser(where))
-        os.remove(os.path.expanduser(new_file_name))
+        new_file_name = newname_if_present(src, where)
+        # new_file_name = basename(new_file_name)
+        print("mvfile " + new_file_name)
+        shutil.move(src, join(expanduser(where), new_file_name))
     else:
-        shutil.move(src, os.path.expanduser(where))
-        os.system(f"rm {os.path.expanduser(src)}")
+        shutil.move(src, join(expanduser(where), basename(src)))
+    # os.system("mv "+os.path.expanduser(src) + " " + os.path.expanduser(where))
 
 
 def check_if_exists(filename: str, where: str) -> bool:
-    filename = os.path.splitext(filename)[-1]
-    is_present = os.path.exists(os.path.join(where, filename))
-    print(is_present)
-    print(os.listdir(os.path.expanduser(where)))
-    return is_present
+    # filename = basename(filename)
+    # is_present = exists(join(where, filename))
+    # print(is_present)
+    # print(filename)
+    # print(os.listdir(expanduser(where)))
+    if basename(filename) in os.listdir(expanduser(where)):
+        return True
+    return False
 
 
-def rename_if_present(filename: str, where: str) -> str:
+def newname_if_present(filename: str, where: str) -> str:
     new_filename = filename
-    i = 0
+    i = 1
     while True:
         if check_if_exists(new_filename, where):
-            filename_without_extension = os.path.splitext(new_filename)[0]
+            filename_without_extension = splitext(new_filename)[0]
             extension = ""
-            for xtension in os.path.splitext(new_filename)[1:]:
+            for xtension in splitext(new_filename)[1:]:
                 extension += xtension
 
             new_filename = "%s(%d)%s" % (filename_without_extension, i, extension)
         else:
             print(new_filename)
-            return new_filename
+            return basename(new_filename)
         i += 1
